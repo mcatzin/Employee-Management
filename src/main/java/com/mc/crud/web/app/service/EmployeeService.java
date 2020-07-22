834d6722c12dc8f548152a2898b2a88a05babe7c
Mario Catzin <catzin21@gmail.com>
2020-07-22 16:00:37 -0700

    delete method completed

--- src/main/java/com/mc/crud/web/app/service/EmployeeService.java
+++ src/main/java/com/mc/crud/web/app/service/EmployeeService.java
@@ -7,4 +7,6 @@ import com.mc.crud.web.app.model.Employee;
 public interface EmployeeService {
 	List<Employee> getAllEmployees();
 	void addEmployee(Employee employee);
+	Employee getEmployeeById(long id);
+	void deleteEmployeeById(long id);
 }
--- src/main/java/com/mc/crud/web/app/service/EmployeeServiceImpl.java
+++ src/main/java/com/mc/crud/web/app/service/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.mc.crud.web.app.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,22 @@ public class EmployeeServiceImpl implements EmployeeService{
 	public void addEmployee(Employee employee) {
 		this.employeeRepository.save(employee);
 		
+	}
+
+	@Override
+	public Employee getEmployeeById(long id) {
+		Optional<Employee> optional = employeeRepository.findById(id);
+		Employee employee = null;
+		if(optional.isPresent()) {
+			employee = optional.get();
+		}else {
+			throw new RuntimeException("Employee is not found for id: " + id);
+		}
+		return employee;
+	}
+
+	@Override
+	public void deleteEmployeeById(long id) {
+		this.employeeRepository.deleteById(id);
 	}
 }
